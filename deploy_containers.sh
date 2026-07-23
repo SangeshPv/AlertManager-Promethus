acd2ad9bc442d9381e77481c62e808ea389286be
SangeshPv <sangeshpv@gmail.com>
2026-07-23 17:09:49 +0200

    fix: Improve comment clarity for container boot wait time

--- deploy_containers.sh
+++ deploy_containers.sh
@@ -30,7 +30,7 @@ incus create images:ubuntu/noble/cloud SNMPExporter
 incus create images:ubuntu/noble/cloud alertmanager
 echo "Starting containers..."
 incus start switch SNMPExporter alertmanager cnt2
-
+#This is to give the containers time to boot and acquire IP addresses this ensures that the IP addresses are available when we try to fetch them later in the script.
 echo "Waiting 20s for containers to boot and acquire IP addresses..."
 sleep 20
 incus list
